1498a8b1363351ae43d70e25bfe722b86f6ffa13
GabrielCruz <ecgmtrabalhos@gmail.com>
2025-04-10 13:55:53 +0100

    update text

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        System.out.println("Vamos colaborar");
+        System.out.println("Boa Tarde\n\n Tudo bem?");
     }
 }
