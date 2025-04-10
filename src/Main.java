390573647690c25b73b9d6f113966ba23211bcf7
Richard <137336853+R1cardoFP@users.noreply.github.com>
2025-04-10 13:51:06 +0100

    alteracao branch ricardo

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("Vamos colaborar");
+
+
+
+     System.out.println("Eu estou a trabalhar ao mesmo tempo");
+
     }
 }
